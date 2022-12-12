52cfa08c51416cc4d4f1e8e9d0d8b3d7a3af429d
saas20035 <89081860+saas20035@users.noreply.github.com>
2022-12-13 00:21:50 +0630

    Update config.py

--- pornhub/config.py
+++ pornhub/config.py
@@ -1,8 +1,8 @@
 from typing import List
 
-API_ID: int = 12345
-API_HASH: str = "abcdefg:12345678"
-TOKEN: str = "abcdefg:12345678"
+API_ID: int = 8080846
+API_HASH: str = "d7715182baf1398e423d1b4b51741679"
+TOKEN: str = "5962972343:AAGnjQPRnAZ1tlhgCnRU5WHmCNw3l57qRbA"
 
 log_chat: int = -12345678
 sub_chat: str = "channel_username"
